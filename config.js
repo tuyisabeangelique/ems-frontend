e8c7c344e9862df499d0ab272e88e95530d007f1
tuyisabeangelique <124276868+tuyisabeangelique@users.noreply.github.com>
2025-01-10 12:59:44 -0500

    handle cors error

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-// const config = {
-//   backendUrl: "http://localhost:5000/api",
-// };
-
 const config = {
   backendUrl: "https://ems-server-brown.vercel.app/api",
   urlNoAPI: "https://ems-server-brown.vercel.app",
 };
+
+// const config = {
+//   backendUrl: "http://localhost:5000/api",
+//   urlNoAPI: "http://localhost:5000",
+// };
 
 export default config;
